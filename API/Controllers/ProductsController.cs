8c5901a1d140a87c412dc1f091f165803ff1c7e2
MosaabAL_Dawood <maldawod@aspire-services.net>
2024-12-22 17:02:05 -0600

    add routes and axios along with details page

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -24,7 +24,7 @@ namespace API.Controllers
             return Ok(products);
         }
 
-        [HttpGet("id")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             return await _storeContext.Products.FindAsync(id);
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,17 +1,87 @@
-import { AppBar, Switch, Toolbar, Typography } from '@mui/material'
+import { ShoppingCart } from '@mui/icons-material'
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from '@mui/material'
+import { NavLink } from 'react-router-dom'
+
+const midLinks = [
+    {title: 'catalog', path: '/catalog'},
+    {title: 'about', path: '/about'},
+    {title: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+    {title: 'login', path: '/login'},
+    {title: 'signup', path: '/signup'},
+]
+
 
 interface Props {
     handleThemeChange: () => void
     darkMode: boolean
 }
+
+const navStyles = {
+    textDecoration: 'none',
+    color: 'inherit', 
+    typography: 'h6',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
+
 function Header({ handleThemeChange,darkMode }: Props) {
     return (
         <AppBar position='static' sx={{ mb: 4 }}>
-            <Toolbar>
-                <Typography variant='h6'>Store</Typography>
-                <Switch onChange={handleThemeChange}
-                    checked={darkMode}
-                />
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                <Box display={'flex'} alignItems={'center'}>
+                    <Typography 
+                        component={NavLink} 
+                        to={'/'} 
+                        variant='h6'
+                        sx={navStyles}
+                    >
+                        Store
+                    </Typography>
+                    <Switch onChange={handleThemeChange}
+                        checked={darkMode}
+                    />
+                </Box>
+
+                <List sx={{display: 'flex'}}>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navStyles}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+                
+                <Box display={'flex'} alignItems={'center'}>
+                    <IconButton size='large' edge='start' color='inherit' sx={{mr: 2}}>
+                        <Badge badgeContent='4' color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+
+                    </IconButton>
+                    <List sx={{display: 'flex'}}>
+                        {rightLinks.map(({title, path}) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
             </Toolbar>
         </AppBar>
     )
